6683ee4ca8672ad113bea589b59469e905ade945
ivonchan <ivon@share.asia>
2024-05-20 21:52:31 +0800

    sync

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import MyApp from './pages/_app';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<MyApp />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
