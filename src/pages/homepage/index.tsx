6965c4d29e78253b2daf4903ed5ac285de037fd3
ivonchan <ivon@share.asia>
2024-05-21 23:58:44 +0800

    fix build

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,21 +1,12 @@
 import type { NextPage } from "next";
-import { useIntl } from "react-intl";
 
 import { useEffect } from "react";
-import ccxt from "ccxt";
 import styles from "../../styles/main.module.scss";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from "react-responsive-carousel";
-import HardwareRecyclingSection from "../../components/Section/Service/HardwareRecyclingSection";
-import ProductListSection from "../../components/Section/Service/ProductListSection";
-import CustomizationServiceSection from "../../components/Section/Service/CustomizationServiceSection";
-import PaymentChannelsSection from "../../components/Section/Service/PaymentChannelsSection";
 import Header from "../../components/Navbars/Header";
 import { SEO } from "../../components/seo";
 import { usePages } from "../../contexts/pages/use-pages";
-import { ContactUsSection } from "../../feature/index/ContactUsSection";
 import { PagesType } from "../../types/pages";
-import { HomeSection } from "../../feature/index/HomeSection";
 import Footer from "../../components/Footers/Footer";
 import ThemeSection from "../../components/Section/ThemeSection";
 import { useTranslation } from "react-i18next";
